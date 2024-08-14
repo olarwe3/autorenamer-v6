918bdae0eb69b138dff9c6317cc60aaffd947442
olarwe3 <112346990+olarwe3@users.noreply.github.com>
2024-08-14 09:43:23 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,102 @@
+<img src="https://graph.org/file/9bf9479159d2036843976.jpg" alt="logo" target="/blank">
+
+<h1 align="center">
+ <b><a href="https://t.me/AutoRenamePro_bot" target="/blank"> Auto Rename Bot </a></>
+
+</h1>
+
+<p align="center">🤍 Thanks for Being Here 🤍</p>
+
+<p align="center"> 🅡🅔🅟🅞 🅢🅣🅐🅣🅢 </p>
+
+
+![github card](https://github-readme-stats.vercel.app/api/pin/?username=AshutoshGoswami24&repo=Auto-Rename-Bot&theme=dark)
+### ⚡️ Configs 
+
+* `BOT_TOKEN`  - Get bot token from @BotFather
+
+* `API_ID` - From my.telegram.org
+
+* `API_HASH` - From my.telegram.org
+
+* `ADMIN` - Bot controllers id's , multiple id use space to split.
+
+* `LOG_CHANNEL` - Bot Log Sending Channel ⚠️ Id startswith -100 must.
+
+* `DB_URL`  - Mongo Database URL from https://cloud.mongodb.com
+
+* `DB_NAME`  - Your database name from mongoDB. `Optional`
+
+* `FORCE_SUB` - Your force sub channel username without @ `Optional`
+
+* `START_PIC` - Start message photo. `Optional`
+
+* `WEBHOOK` - If your server is need web service! value = True else value = False. `Optional`
+
+  ### 📶 DEPLOYEMENT SUPPORT
+
+<details><summary>Deploy To Koyeb</summary>
+<p>
+<br>                 
+<a target="/blank" href="https://app.koyeb.com/deploy?type=git&repository=github.com/AshutoshGoswami24/Auto-Rename-Bot&branch=main&name=PandaXAshu" >
+  <img src="https://www.koyeb.com/static/images/deploy/button.svg" alt="Deploy">
+</a>
+</p>
+</details>
+
+<details><summary>Deploy To Heroku</summary>
+<p>
+<br>
+<a href="https://heroku.com/deploy?template=https://github.com/AshutoshGoswami24/Auto-Rename-Bot">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
+</p>
+</details>
+
+
+
+
+
+#### 🥰 Features
+ - Renames very fast .
+ - Permanent Thumbnail support.
+ - Force join for the user for use.
+ - Supports Broadcasts.
+ - Set custom caption.
+ - Has a custom Start-up pic.
+ - Force subscribe available.
+ - Supports ulimited renaming at a time.
+ - Deploy to Koyeb + Heroku + Railway.
+ - Automatically rename your files
+ - Set mediatype to upload filetype
+ - Developer Service 24x7. 🔥
+
+
+
+### 🚦 User Commands
+```
+start - Check if the bot is running.
+autorename - To auto rename your files.
+tutorial - SETUP AUTO RENAME FORMAT 
+setmedia - To set your media type preference.
+start - To know how to use me.
+viewthumb - To view current thumbnail.
+delthumb - To delete current thumbnail.
+set_caption - set a custom caption.
+see_caption - see your custom caption.
+del_caption - delete custom caption.
+restart - To restart the bot [FOR ADMINS USE ONLY]
+broadcast - Message Broadcast command [FOR ADMINS USE ONLY].
+status - Check bot status [FOR ADMINS USE ONLY].
+```
+
+
+
+#### 🧡 Respecting... 🧡
+- [🔥PandaWep](https://github.com/PandaWep) 
+- [🔥AshutoshGoswami24](https://github.com/AshutoshGoswami24)
+
+### 🤩 Bots Channel
+<a href="https://t.me/PandaWep">
+   <p>❣️PandaWep🔥</p>
+</a>
---
+++ config.py
@@ -0,0 +1,105 @@
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "Your_api_id")
+    API_HASH  = os.environ.get("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token") 
+
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","Howtogoogle")     
+    DB_URL  = os.environ.get("DB_URL","your_mongo_url")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002204833964") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002155664087"))
+    PORT = int(os.environ.get("PORT", "8080"))
+    
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+
+
+class Txt(object):
+    # part of text configuration
+        
+    START_TXT = """Hello {} 
+    
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+    
+➻ Using This Bot You Can Auto Rename Of Your Files.
+    
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
+    
+➻ Use /tutorial Command To Know How To Use Me.
+
+<b>Bot Is Made By @how_to_google</b>
+
+<b><a href='https://github.com/</a></b>
+"""
+    
+    FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
+
+Use These Keywords To Setup Custom File Name
+
+✓ episode :- To Replace Episode Number
+✓ quality :- To Replace Video Resolution
+
+<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @how_to_google </code>
+
+<b>➻ Your Current Auto Rename Format :</b> <code>{format_template}</code> """
+    
+    ABOUT_TXT = f"""<b>🤖 My Name :</b>
+<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
+<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
+<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
+<b>📢 Channel :</b> <a href='https://t.me/how_to_google'>HowToGoogle</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/lkd_ak'>lkd_ak</a>
+    
+<b>♻️ Bot Made By :</b> @how_to_google"""
+
+    
+    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+    
+⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....
+    
+⦿ /viewthumb - Use This Command To See Your Thumbnail
+⦿ /delthumb - Use This Command To Delete Your Thumbnail"""
+
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    
+⦿ /set_caption - Use This Command To Set Your Caption
+⦿ /see_caption - Use This Command To See Your Caption
+⦿ /del_caption - Use This Command To Delete Your Caption"""
+
+    PROGRESS_BAR = """<b>\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ Eᴛᴀ: {4}
+┣⪼ 🥺 joine Plz: @how_to_google
+╰━━━━━━━━━━━━━━━➣ </b>"""
+    
+    
+    DONATE_TXT = """<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+    
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+    
+<b>My UPI - aashishpatidar </b> """
+    
+    HELP_TXT = """<b>Hey</b> {}
+    
+Joine @how_to_google To Help """
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @PandaWep
+# Developer @AshutoshGoswami24
